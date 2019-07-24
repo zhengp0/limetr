bb8e873efa634c905362fb78fd1c8b4b4224551e
zhengp0 <zhengp@uw.edu>
2019-07-23 20:38:03 -0700

    version: advance to version 0.0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from numpy.distutils.core import setup
 from numpy.distutils.core import Extension
 
 # fortran extension module
-ext = Extension(name='limetr.futils',
-                sources=['limetr/utils.f90'],
+ext = Extension(name='limetr.special_mat',
+                sources=['limetr/special_mat.f90'],
                 library_dirs=['/usr/local/lib'],
                 libraries=['lapack', 'blas'])
 
 setup(name='limetr',
-      version='0.0.1',
+      version='0.0.2',
       description='linear mixed effects model with trimming',
       url='https://github.com/zhengp0/limetr',
       author='Peng Zheng',
