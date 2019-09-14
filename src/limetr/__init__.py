d1145e6811bc492e5641fd04108d4d342f904284
zhengp0 <zhengp@uw.edu>
2019-09-14 14:31:13 -0700

    fix: fix the estimation of the random effects

--- src/limetr/__init__.py
+++ src/limetr/__init__.py
@@ -527,11 +527,31 @@ class LimeTr:
             print('Please fit the model first.')
             return None
 
-        R = self.Y - self.F(self.beta)
-        r = np.split(R, np.cumsum(self.n)[:-1])
-        z = np.split(self.Z, np.cumsum(self.n)[:-1], axis=0)
+        if self.std_flag == 0:
+            S = self.S
+        elif self.std_flag == 1:
+            S = np.sqrt(np.repeat(self.delta[0], self.N))
+        elif self.std_flag == 2:
+            S = np.sqrt(np.repeat(self.delta, self.n))
 
-        u = [np.linalg.solve(z[i].T.dot(z[i]), z[i].T.dot(r[i]))
+        if self.use_trimming:
+            R = (self.Y - self.F(self.beta))*np.sqrt(self.w)
+            Z = self.Z*(np.sqrt(self.w).reshape(self.N, 1))
+        else:
+            R = self.Y - self.F(self.beta)
+            Z = self.Z
+
+        iV = 1.0/S**2
+        iVZ = Z*iV.reshape(iV.size, 1)
+        igamma = 1.0/self.gamma
+
+        r = np.split(R, np.cumsum(self.n)[:-1])
+        z = np.split(Z, np.cumsum(self.n)[:-1], axis=0)
+        ivz = np.split(iVZ, np.cumsum(self.n)[:-1], axis=0)
+        
+
+        u = [np.linalg.solve(ivz[i].T.dot(z[i]) + np.diag(igamma),
+                             ivz[i].T.dot(r[i]))
              for i in range(self.m)]
 
         self.u = np.vstack(u)
