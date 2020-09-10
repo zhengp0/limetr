1c6939b2fba0bfb08dddaa051829d135591e736d
zhengp0 <zhengp@uw.edu>
2020-09-10 09:38:28 -0700

    make the distributions flexible with the size.

--- src/limetr/stats/distribution.py
+++ src/limetr/stats/distribution.py
@@ -4,20 +4,41 @@
 
     Distribution module.
 """
-from dataclasses import dataclass
+from typing import Any, List
+from collections.abc import Iterable
+from dataclasses import dataclass, field
 import numpy as np
+
+
+def default_attr_factory(attr: Any, size: int, default_value: float,
+                         attr_name: str = 'attr') -> np.ndarray:
+    if attr is None:
+        attr = np.repeat(default_value, size)
+    elif np.isscalar(attr):
+        attr = np.repeat(attr, size)
+    else:
+        attr = np.asarray(attr)
+        assert len(attr) == size, f"{attr_name} must be a scalar or has length {size}."
+
+    return attr
+
+
+def isiterable(__obj: object) -> bool:
+    return isinstance(__obj, Iterable)
 
 
 @dataclass
 class Distribution:
-    size: int = 1
+    size: int = field(default=None, repr=False)
 
-    def __post_init__(self):
+    def process_size(self, attrs: List[Any]):
+        if self.size is None:
+            sizes = [len(attr) for attr in attrs if isiterable(attr)]
+            sizes.append(1)
+            self.size = max(sizes)
+
         if self.size <= 0 or not isinstance(self.size, int):
             raise ValueError("Size of the distribution must be a positive integer.")
-
-    def _check_attr(self):
-        raise NotImplementedError("Please do not directly use Distribution class.")
 
 
 @dataclass
@@ -26,15 +47,10 @@ class Gaussian(Distribution):
     sd: np.ndarray = None
 
     def __post_init__(self):
-        super().__post_init__()
-        self.mean = np.zeros(self.size) if self.mean is None else self.mean
-        self.sd = np.repeat(np.inf, self.size) if self.sd is None else self.sd
-        self._check_attr()
-
-    def _check_attr(self):
-        assert len(self.mean) == self.size, f"mean should have length {self.size}"
-        assert len(self.sd) == self.size, f"sd should have length {self.size}"
-        assert all(self.sd > 0.0), "SD should be all positive."
+        self.process_size([self.mean, self.sd])
+        self.mean = default_attr_factory(self.mean, self.size, 0.0, attr_name='mean')
+        self.sd = default_attr_factory(self.sd, self.size, np.inf, attr_name='sd')
+        assert all(self.sd > 0.0), "Standard deviation must be all positive."
 
 
 @dataclass
@@ -43,15 +59,10 @@ class Uniform(Distribution):
     ub: np.ndarray = None
 
     def __post_init__(self):
-        super().__post_init__()
-        self.lb = np.repeat(-np.inf, self.size) if self.lb is None else self.lb
-        self.ub = np.repeat(np.inf, self.size) if self.ub is None else self.ub
-        self._check_attr()
-
-    def _check_attr(self):
-        assert len(self.lb) == self.size, f"lb should have length {self.size}"
-        assert len(self.ub) == self.size, f"ub should have length {self.size}"
-        assert all(self.lb <= self.ub), f"lb should be less or equal than ub."
+        self.process_size([self.lb, self.ub])
+        self.lb = default_attr_factory(self.lb, self.size, -np.inf, attr_name='lb')
+        self.ub = default_attr_factory(self.ub, self.size, np.inf, attr_name='ub')
+        assert all(self.lb <= self.ub), "Lower bounds must be less or equal than upper bounds."
 
 
 @dataclass
@@ -60,12 +71,7 @@ class Laplace(Distribution):
     sd: np.ndarray = None
 
     def __post_init__(self):
-        super().__post_init__()
-        self.mean = np.zeros(self.size) if self.mean is None else self.mean
-        self.sd = np.repeat(np.inf, self.size) if self.sd is None else self.sd
-        self._check_attr()
-
-    def _check_attr(self):
-        assert len(self.mean) == self.size, f"mean should have length {self.size}"
-        assert len(self.sd) == self.size, f"sd should have length {self.size}"
-        assert all(self.sd > 0.0), "SD should be all positive."
+        self.process_size([self.mean, self.sd])
+        self.mean = default_attr_factory(self.mean, self.size, 0.0, attr_name='mean')
+        self.sd = default_attr_factory(self.sd, self.size, np.inf, attr_name='sd')
+        assert all(self.sd > 0.0), "Standard deviation must be all positive."
