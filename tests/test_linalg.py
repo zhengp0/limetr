4d444608aba4646c34e47eb5297cafa393c0656b
Peng Zheng <zhengp@uw.edu>
2021-01-05 15:23:02 -0800

    Add the doc and test for linalg module (#11)
    * add doc for smooth_mapping module
    
    * complete the doc for linear mapping
    
    * add test for smoothing mapping

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -7,24 +7,6 @@ import limetr.linalg as linalg
 #pylint: disable=redefined-outer-name
 
 
-nblocks = 5
-nrows = np.random.randint(1, 5, nblocks)
-ncols = np.random.randint(1, 5, nblocks)
-
-
-@pytest.fixture
-def bdmat():
-    mat_blocks = [np.random.randn(nrows[i], ncols[i]) for i in range(nblocks)]
-    return linalg.BlockDiagMat(mat_blocks)
-
-
-@pytest.fixture
-def sbdmat():
-    mat_blocks = [np.random.randn(ncols[i], ncols[i]) for i in range(nblocks)]
-    mat_blocks = [mat.T.dot(mat) for mat in mat_blocks]
-    return linalg.SquareBlockDiagMat(mat_blocks)
-
-
 @pytest.fixture
 def smooth_mapping():
     m = 5
@@ -47,55 +29,6 @@ def linear_mapping():
     n = 2
     mat = np.random.randn(m, n)
     return linalg.LinearMapping(mat)
-
-
-@pytest.mark.parametrize('vec', [np.ones(ncols.sum()),
-                                 np.random.randn(ncols.sum())])
-def test_bd_dot(bdmat, vec):
-    fullmat = bdmat.full()
-    assert np.allclose(fullmat.dot(vec), bdmat.dot(vec))
-
-
-@pytest.mark.parametrize('array', [np.ones(ncols.sum()),
-                                   np.random.randn(ncols.sum()),
-                                   np.random.randn(ncols.sum(), 2)])
-def test_sbd_dot(sbdmat, array):
-    fullmat = sbdmat.full()
-    assert np.allclose(fullmat.dot(array), sbdmat.dot(array))
-
-
-def test_sbd_inv(sbdmat):
-    fullmat = sbdmat.full()
-    invmat = sbdmat.inv()
-    fullinvmat = invmat.full()
-    assert np.allclose(np.linalg.inv(fullmat), fullinvmat)
-
-
-@pytest.mark.parametrize('array', [np.ones(ncols.sum()),
-                                   np.random.randn(ncols.sum()),
-                                   np.random.randn(ncols.sum(), 2)])
-def test_sbd_invdot(sbdmat, array):
-    fullinvmat = sbdmat.inv().full()
-    assert np.allclose(fullinvmat.dot(array), sbdmat.invdot(array))
-
-
-def test_sbd_diag(sbdmat):
-    fullmat = sbdmat.full()
-    assert np.allclose(np.diag(fullmat), sbdmat.diag())
-
-
-def test_sbd_block_eigvals(sbdmat):
-    assert np.allclose(np.hstack([np.linalg.eigvals(mat) for mat in sbdmat.mat_blocks]), sbdmat.block_eigvals())
-
-
-def test_sbd_det(sbdmat):
-    fullmat = sbdmat.full()
-    assert np.isclose(np.linalg.det(fullmat), sbdmat.det())
-
-
-def test_sbd_logdet(sbdmat):
-    fullmat = sbdmat.full()
-    assert np.isclose(np.linalg.slogdet(fullmat)[1], sbdmat.logdet())
 
 
 def test_smooth_mapping(smooth_mapping):
